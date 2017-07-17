6b7e6eb12efb8e281c395ebf84941769665d5096
Greg Keele <gkeele@gmail.com>
2017-07-17 18:14:09 -0400

    Add genome cache converter for DOQTL additive probabilities

--- R/calc.kinship.from.genomecache.with.DOQTL.R
+++ R/calc.kinship.from.genomecache.with.DOQTL.R
@@ -1,23 +1,24 @@
 #' @export
 calc.kinship.from.genomecache.with.DOQTL <- function(genomecache, model="additive"){
   h <- DiploprobReader$new(genomecache)
-  mapping <- straineff.mapping.matrix()
+  #mapping <- straineff.mapping.matrix()
   
   probs <- array(NA, dim=c(length(h$getSubjects()), length(h$getFounders()), length(h$getLoci())))
   
   loci <- h$getLoci()
   
   for(i in 1:length(loci)){
-    diplotypes <- h$getLocusMatrix(loci[i], model="full")
-    if(any(diplotypes < 0)){
-      diplotypes[diplotypes < 0] <- 0
-      diplotypes <- t(apply(diplotypes, 1, function(x) x/sum(x)))
-    }
+    #diplotypes <- h$getLocusMatrix(loci[i], model="full")
+    #if(any(diplotypes < 0)){
+    #  diplotypes[diplotypes < 0] <- 0
+    #  diplotypes <- t(apply(diplotypes, 1, function(x) x/sum(x)))
+    #}
     if(model == "additive"){
-      use.probs <- (diplotypes %*% mapping)/2
+      #use.probs <- (diplotypes %*% mapping)/2
+      use.probs <- h$getLocusMatrix(loci[i], model="full")/2
     }
     if(model == "full"){
-      use.probs <- diplotypes
+      use.probs <- h$getLocusMatrix(loci[i], model="full")
     }
     probs[,,i] <- use.probs
   }
