8cd90ed01cd0ae22720916a73790d5ed0c803b18
Greg Keele <gkeele@gmail.com>
2017-09-09 22:30:54 -0400

    Made genome.plotter.whole()'s handling of polygon much more efficient

--- R/genome_plotter.R
+++ R/genome_plotter.R
@@ -73,8 +73,8 @@ genome.plotter.to.pdf <- function(scan.object, chr=c(1:19, "X"), use.lod=FALSE,
 genome.plotter.chr <- function(scan.object, chr, use.lod=FALSE,
                                scale=c("Mb", "cM"), main.col="black", median.band.col="gray88",
                                main="", no.title=FALSE, override.title=NULL, 
-                               my.y.line=2, my.y.axis.cex=1, y.max.manual=NULL, 
-                               my.x.line=2, my.x.axis.cex=1,
+                               my.y.line=2, my.y.axis.cex=1, y.max.manual=NULL, make.y.axis.sparse=FALSE, my.ylab.cex=1,
+                               my.x.line=2, my.x.axis.cex=1, my.x.labels=TRUE, override.xlab=NULL, my.xlab.cex=1,
                                my.legend.cex=0.6, my.type="l", point.cex=0.5,
                                hard.thresholds=NULL, thresholds.col="red", thresholds.legend=NULL,
                                include.qtl.rug=FALSE, rug.pos=NULL, rug.col="gray50",
@@ -143,11 +143,19 @@ genome.plotter.chr <- function(scan.object, chr, use.lod=FALSE,
        ylim=c(0, y.max), 
        xaxt="n", yaxt="n", xlab="", ylab="", main=this.title,
        frame.plot=FALSE, type=my.type, cex=point.cex, lwd=1.5, col=main.col, pch=20)
-  axis(side=2, at=0:y.max, las=2, cex.axis=my.y.axis.cex)
-  mtext(text=this.ylab, side=2, line=my.y.line)
   
-  axis(side=1, cex.axis=my.x.axis.cex)
-  mtext(text=paste("Chr", chr, paste0("(", scale, ")")), side=1, line=my.x.line)
+  if(!make.y.axis.sparse){
+    axis(side=2, at=0:y.max, las=2, cex.axis=my.y.axis.cex)
+  }
+  else{
+    axis(side=2, las=2, cex.axis=my.y.axis.cex)
+  }
+  mtext(text=this.ylab, side=2, line=my.y.line, cex=my.ylab.cex)
+  
+  this.xlab <- ifelse(is.null(override.xlab), paste("Chr", chr, paste0("(", scale, ")")), override.xlab)
+  axis(side=1, cex.axis=my.x.axis.cex, labels=my.x.labels)
+  mtext(text=this.xlab, side=1, line=my.x.line, cex=my.xlab.cex)
+  
   if(!is.null(CI)){
     polygon(x=c(pos, rev(pos)), y=c(CI[1,], rev(CI[2,])), density=NA, col=median.band.col)
   }
@@ -155,7 +163,10 @@ genome.plotter.chr <- function(scan.object, chr, use.lod=FALSE,
 
   if(include.qtl.rug){
     if(is.null(rug.pos)){ rug.pos <- pos[which.max(outcome)] }
-    rug(rug.pos, col=rug.col, ticksize=0.06, lwd=2.5)
+    if(length(rug.col) == 1){ rug.col <- rep(rug.col, length(rug.pos))}
+    for(i in 1:length(rug.pos)){
+      rug(rug.pos[i], col=rug.col[i], ticksize=0.06, lwd=2.5)
+    }
   }
   
   if(!is.null(hard.thresholds)){
