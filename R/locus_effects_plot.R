8aa5aa135df647d1075008ee9e1e1c8b1a45332a
Greg Keele <gkeele@gmail.com>
2018-02-05 20:16:24 -0500

    Updated options for plot_locus.effect.from.scan()

--- R/locus_effects_plot.R
+++ R/locus_effects_plot.R
@@ -11,6 +11,11 @@
 #' @param col DEFAULT: c(rgb(240, 240, 0, maxColorValue=255), rgb(128, 128, 128, maxColorValue=255), rgb(240, 128, 128, maxColorValue=255), 
 #' rgb(16, 16, 240, maxColorValue=255), rgb(0, 160, 240, maxColorValue=255), rgb(0, 160, 0, maxColorValue=255), rgb(240, 0, 0, maxColorValue=255), 
 #' rgb(144, 0, 224, maxColorValue=255)). These are the established Collaborative Cross colors.
+#' @param add.connect.line DEFAULT: TRUE. Adds a connection line between the effects (or their mean).
+#' @param connect.lwd DEFAULT: 2. The line width of the connection line, if it is included
+#' @param connect.col DEFAULT: "black". The color of the connection line, if it is included
+#' @param add DEFAULT: FALSE. If TRUE, a new plot is not initiated. The purpose is to allow multiple sets of
+#' effects to be included on a single plot.
 #' @export
 #' @examples plot_locus.effect.from.scan()
 plot_locus.effect.from.scan <- function(scan.object, locus,
@@ -23,23 +28,41 @@ plot_locus.effect.from.scan <- function(scan.object, locus,
                                               rgb(0, 160, 240, maxColorValue =255),
                                               rgb(0, 160, 0, maxColorValue =255),
                                               rgb(240, 0, 0, maxColorValue = 255), 
-                                              rgb(144, 0, 224, maxColorValue = 255))){
+                                              rgb(144, 0, 224, maxColorValue = 255)),
+                                        add.connect.line=TRUE, connect.lwd=2, connect.col="black",
+                                        add=FALSE,
+                                        ...){
   allele.effects <- scan.object$allele.effects
-  effect.matrix <- allele.effects[,locus,]
   
-  if(length(col) == 1){ col <- rep(col, nrow(effect.matrix)) }
-  
-  effects.95 <- t(apply(effect.matrix, 1, function(x) miqtl::ci.mean(x, alpha=0.05)))
-  effects.75 <- t(apply(effect.matrix, 1, function(x) miqtl::ci.mean(x, alpha=0.25)))
-  mean.effects <- apply(effect.matrix, 1, function(x) mean(x, alpha=0.05, na.rm=TRUE))
-
-  plot.ci(midvals=mean.effects, narrow.intervals=effects.75, wide.intervals=effects.95,
-          names=names, col=col)   
+  if (length(dim(allele.effects)) == 3) {
+    effect.matrix <- allele.effects[,locus,]
+    
+    effects.95 <- t(apply(effect.matrix, 1, function(x) miqtl::ci.mean(x, alpha=0.05)))
+    effects.75 <- t(apply(effect.matrix, 1, function(x) miqtl::ci.mean(x, alpha=0.25)))
+    mean.effects <- apply(effect.matrix, 1, function(x) mean(x, alpha=0.05, na.rm=TRUE))
+    
+    if (length(col) == 1) { col <- rep(col, nrow(effect.matrix)) }
+    
+    plot.ci(midvals=mean.effects, narrow.intervals=effects.75, wide.intervals=effects.95,
+            names=names, col=col, add=add, connect.col=connect.col, ...) 
+  }
+  else if (length(dim(allele.effects)) == 2) {
+    effect.vector <- allele.effects[,locus]
+    
+    effects.95.junk <- cbind(effect.vector, effect.vector)
+    
+    if (length(col) == 1) { col <- rep(col, length(effect.vector)) }
+    
+    plot.ci(midvals=effect.vector, narrow.intervals=effects.95.junk, wide.intervals=effects.95.junk,
+            names=names, col=col, add=add, connect.col=connect.col, ...) 
+  }
   abline(v=0, lty=2)
   title(main)
 }
 
-plot.ci <- function(midvals, narrow.intervals, wide.intervals,
+plot.ci <- function(midvals, 
+                    narrow.intervals, 
+                    wide.intervals,
                     names=1:length(midvals),
                     add=FALSE,
                     xlab="Estimate", xlab.line=2.5, x.cex=1, x.padj=1, x.labels=TRUE, xlab.cex=1,
@@ -54,6 +77,7 @@ plot.ci <- function(midvals, narrow.intervals, wide.intervals,
                     type="p",
                     use.this.lim=NULL,
                     main="",
+                    add.connect.line=TRUE, connect.lwd=2, connect.col="black",
                     ...)
 {
   nvals <- length(midvals)
@@ -77,11 +101,15 @@ plot.ci <- function(midvals, narrow.intervals, wide.intervals,
     }
   }
   if("p"==type){
+    pos <- nvals - 1:nvals + 0.5 + shift
+    if (add.connect.line) {
+      lines(midvals, pos, col=connect.col, lwd=connect.lwd)
+    }
     for(i in 1:nvals){
-      pos <- nvals-i + 0.5 + shift
-      lines(wide.intervals[i,], rep(pos,2), col=col[i])
-      lines(narrow.intervals[i,], rep(pos,2), lwd=3, col=col[i])
-      points(midvals[i], pos, pch=pch.midvals, col=col.midvals[i], cex=point.cex)
+      #pos <- nvals-i + 0.5 + shift
+      lines(wide.intervals[i,], rep(pos[i],2), col=col[i])
+      lines(narrow.intervals[i,], rep(pos[i],2), lwd=3, col=col[i])
+      points(midvals[i], pos[i], pch=pch.midvals, col=col.midvals[i], cex=point.cex)
     }
   }
   invisible(rev(y.pos))
